834fc42a51d1c10496c63a4d2e3a208fb932c35b
Ryan Rickgauer <rrickgauer1@gmail.com>
2023-07-27 09:10:59 -0500

    remove typing.Self object

---
+++ src/flasklib/__init__.py
@@ -0,0 +1,7 @@
+from . import mappers as mappers
+from . import dates as dates
+from . import errors as errors
+from . import jinja as jinja
+from . import json as json
+from . import responses as responses
+from . import startup as startup
